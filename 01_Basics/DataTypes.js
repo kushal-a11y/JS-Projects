548bd895bdfc2fe76daca12947b2e1afe1b47eb4
kushal-a11y <kushalstcet666@gmail.com>
2025-01-30 13:03:25 +0000

    datattypes

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,37 @@
+// primitive DataTypes
+
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+//Dynamic or Static Type (Does user give type of score machoine understands that during riuntime)
+
+// const score = 100
+const scoref = 100.77
+
+
+// const id = Symbol('123')
+
+// const anId = Symbol('123')
+
+// console.log(id === anId);
+
+// const bigNumber = 674387545675657656754n
+
+// console.log(typeof bigNumber);
+
+
+//Non-primitive or Reference
+
+//Arrays,objects, Functions
+let arr = [1,2,3,4,44]
+let myDetails={
+    name: "Kushal",
+    age: 11,
+}
+
+const myFunction = function(){
+    console.log("Kala Kutta Bhu Bhou");
+}
+
+console.log(typeof myFunction)
+console.log(typeof myDetails)
