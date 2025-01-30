b78919d017f15117c0330f2940be43902edc0718
kushal-a11y <kushalstcet666@gmail.com>
2025-01-30 11:57:09 +0000

    DataTypes

---
+++ 01_Basics/01_dataTypes.js
@@ -0,0 +1,34 @@
+"Use scrict"; //Treat this as newer version
+
+// alert(3 + 3) // Error: we are using nodeJS not Browser
+
+console.log(3 + 3 );
+//Code readability should be high
+console.log("Kushal");
+
+
+let name = null
+let age = 19
+
+let isLoggedin = false
+
+//number => 2 to power 53
+//bigint
+
+// string => ""
+
+//  boolean => true/false
+
+//  null => standalone value (type)
+
+//  undefined => 
+
+//  symbol => uniqueness
+
+
+// object 
+
+
+console.log(typeof null);//object
+
+console.log(typeof undefined);//undefined
