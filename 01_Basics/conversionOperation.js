25116c00cfcaa03f7b6853608ac3d483052ede06
kushal-a11y <kushalstcet666@gmail.com>
2025-01-30 12:21:58 +0000

    TypeConversion

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueinNumber =  Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+
+//Number can be converted eality
+
+//33abc fails => NaN
+
+//True/false => 0/1
+
+//1 => boolean true done
+
+// let isLoggedIn = 0
+
+// let booleanLoggedIn = Boolean(isLoggedIn)
+
+let num = 22
+
+let numToString = String(num)
+
+
+console.log(typeof numToString);
